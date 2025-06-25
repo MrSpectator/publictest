652c69276e810d951c2c96a28944adbf3a6697c0
RDAS-SOLUTIONS-LIMITED <sobandeoluwatonie@gmail.com>
2025-06-25 18:18:36 +0100

    deleted

--- app/Modules/Email/Routes/api.php
+++ app/Modules/Email/Routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Modules\Email\Controllers\EmailController;
-
+ 
 Route::prefix('email')->group(function () {
     Route::post('send', [EmailController::class, 'send']);
     Route::get('logs', [EmailController::class, 'logs']);
